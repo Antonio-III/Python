5bfbf4530ec45e6c8d3f01de90ea34863a792de5
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-07-02 15:32:55 +0400

    Add: Sales by Match solution

---
+++ HackerRank/sales_by_match.py
@@ -0,0 +1,35 @@
+# My solution for: https://www.hackerrank.com/challenges/sock-merchant/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    unique_socks = set(ar)
+    return sum([ar.count(unique_sock)//2 for unique_sock in unique_socks])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
