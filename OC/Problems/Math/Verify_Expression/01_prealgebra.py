1b108d27a6920579e4cef2983bb0f87c416735d8
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2026-02-26 10:57:33 +0400

    Add: script to verify algebraic equations

---
+++ OC/Problems/Math/Verify_Expression/01_prealgebra.py
@@ -0,0 +1,54 @@
+"""
+Docstring for OC.Problems.Math.Verify_Expression.01_prealgebra
+
+I wrote this script so I can verify my solutions to pre-algebraic equations having to do it mentally.
+"""
+
+def main():
+    exp = input("Enter expression:\n")
+    var = input("Enter variable character:\n")
+    var_c = input("Enter found value:\n")
+
+    new = rewrite(exp, var, var_c)
+
+    print(new)
+    print(eval(new))
+
+def rewrite(exp: str, var: str, var_c: str) -> str:
+    """
+    Rewrites a mathematical expression where the value of the variable is plugged in. 
+    
+    :param exp: The mathematical expression.
+    :type exp: str
+    :param var: The letter representing the unknown value.
+    :type var: str
+    :param var_c: The value found after solving the equation.
+    :type var_c: str
+    :return: A reformatted expression of the mathematical expression where the solution has been plugged in, to be passed to `eval()` function.
+    :rtype: str
+    """
+    # Make an empty string.
+    new = ""
+    
+    # Loop through original string.
+    for i, char in enumerate(exp):
+        
+        # When encountering variable term with no coeff, add a 1 before copying the term.
+        if char == var_c:
+            
+            # There are 2 places where a coeff-less variable might appear: in the beginning or in the middle of the expression:
+            # If it's in the beginning, we add a 1 to variable because if it had a coeff, it would be in the middle of the expression.
+            # If it's in the middle, we check for the previous char to see if the variable is indeed coeff-less.
+            if i == 0 or not (exp[i-1].isnumeric()):
+                new += "1"  
+        
+        # Copy the characer to new string.
+        new += char
+    
+    # In python, equality symbol is "==" as opposed to "=".
+    new = new.replace("=", "==")
+    
+    # Replace any instances of the variable with the found value.
+    new = new.replace("y", f"*({var})")    
+
+    return new
