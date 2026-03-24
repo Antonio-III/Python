ed92d9801deac1b6bebade0595f6ed1629da483e
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2026-03-24 11:24:52 +0400

    Add feature to simplify fractions - Add divisor attribute to term class (TBC) - Fractional terms are reduced to lowest terms upon dividing. - Coefficients can only be integers

--- OC/Problems/Math/Verify_Expression/02_algebra1.py
+++ OC/Problems/Math/Verify_Expression/02_algebra1.py
@@ -7,27 +7,39 @@ def main():
     # if (not (vars := input("Enter variables (including exponents):\n"))):
 
     #     raise ValueError("Invalid input.")
-    t1 = Term("2", "x^3y^9z^2")
-    t2 = Term("-7", "x^7y^4z^3")
-    t3 = Term("2", "x^6y^3z^7")
-    t1.multiply(t2)
-    t1.multiply(t3)
+    t1 = term("12", "z^7")
+    t2 = term("-6", "z^5")
+    t1.divide(t2)
+
     print(t1.str_form())
 
-class Term():
-    def __init__(self, coeff_str: str = "", vars_str: str = ""):
-        self.coeff = self.__parse_coeff(coeff_str)
-        self.vars = self.__parse_vars(vars_str)
+    t1.add(t2)
+
+class term():
+    """Data type to represent a product/quotient of fininite coefficients and variables.
+    """
+    def __init__(self, coeff: int = 1, vars: dict[str, int] = {}, div: "term | int" = 1):
+        self.coeff = coeff
+        self.vars = vars
+        self.div = div
 
     def str_form(self) -> str:
-        return f"{self.__get_coeff()}{self.__get_variables()}"
+        """Returns the term in its string form.
+        """
+        numerator = f"{self.coeff}{self.__convert_vars_to_str()}"
+        denominator = 1
 
-    def __get_coeff(self) -> int | float:
-        int_coeff = int(self.coeff)
+        if (isinstance(self.div, "term")):
+            denominator = f"{self.div.str_form()}"
 
-        return (int_coeff if (self.coeff == int_coeff) else self.coeff)
+        if denominator != 1:
+            return f"({numerator})/{denominator}"
 
-    def __get_variables(self) -> str:
+        return f"{numerator}"
+
+    def __convert_vars_to_str(self) -> str:
+        """Returns the variables in string form.
+        """
         vars_str = ""
         for v in self.vars.keys():
             # Show variables AND their exponents 
@@ -37,88 +49,18 @@ class Term():
                 vars_str += f"{v}"
 
         return vars_str
-    
-    def __record_vars(self, vars_str: str) -> tuple[list[str], list[int]]:
-        """Step 1 of parsing the variables from a string. Records variables and exponents into separate lists.
 
-        Args:
-            vars_str: The Term's variables in string form.
-
-        Returns:
-            A list of variables and a list of their corresponding exponents.
-        """
-
-        variables_l = []
-        exponents_l = []
-
-        vars_len = len(vars_str)
-
-        exponent_counter = ""
-        
-        for i in range(vars_len):
-            curr = vars_str[i]
-
-            if curr.isalpha():
-                variables_l.append(curr)
-
-                if exponent_counter:
-                    exponents_l.append(int(exponent_counter))
-                    exponent_counter = ""
-
-                if (i+1 == vars_len) or (vars_str[i+1] != "^"):
-                    exponents_l.append(1)
-
-
-            elif curr.isnumeric():
-                exponent_counter += curr
-
-                if (i+1 == vars_len):
-                    exponents_l.append(int(exponent_counter))
-
-        return variables_l, exponents_l
-
-    def __parse_vars(self, vars_str: str) -> dict[str, int]:
-        """Step 2 (last) of parsing the variables from a string. Creates a dict to store the variables and their respective exponents. The dict is sorted.
-
-        Args:
-            vars_str: The Term's variables in string form.
-
-        Returns:
-            The dictionary contaning the variables (Keys) and their exponents (Value).
-        """
-        vars_d = {}
-        variables_l, exponents_l = self.__record_vars(vars_str)
-
-        for v, e in zip(variables_l, exponents_l):
-            vars_d[v] = e
-
-        return dict(sorted(vars_d.items()))
-
-    def __parse_coeff(self, coeff_str: str) -> int | float:
-        """Step 1 (Last) of parsing the coefficient.
-
-        Args:
-            coeff_str: The Term's coefficient in string form.
-
-        Returns:
-            The coefficient in int or float form.
-        """
-        int_coeff = int(coeff_str)
-        float_coeff = float(coeff_str)
-
-        return (int_coeff if (int_coeff == float_coeff) else float_coeff)
-    
     # Only perform on like terms
-    def add(self, addend: "Term"):
+    def add(self, addend: "term"):
         if self.vars == addend.vars:
             self.coeff += addend.coeff
         
-    def subtract(self, subtrahend: "Term"):
+    def subtract(self, subtrahend: "term"):
         if self.vars == subtrahend.vars:
             self.coeff -= subtrahend.coeff
     # ---
 
-    def multiply(self, factor: "Term"):
+    def multiply(self, factor: "term"):
         self.coeff *= factor.coeff
 
         for v in factor.vars:
@@ -127,15 +69,158 @@ class Term():
             else:
                 self.vars[v] = factor.vars[v]
 
-    def divide(self, divisor: "Term"):
-            self.coeff /= divisor.coeff
+    def divide(self, divisor: "term"):
+            # Simplify expressions to lowest terms.
+            self.coeff, divisor.coeff = __simplify_fraction(self.coeff, divisor.coeff)
+            q1 = self.coeff/divisor.coeff
+            q2 = self.coeff//divisor.coeff
+            
+            # Division only occurs between divisible numbers.
+            if (q1 == q2):
+                self.coeff = q2
 
+            # Divide the exponents.
             for v in divisor.vars:
-                if (self.vars.get(v)):
-                    self.vars[v] -= divisor.vars[v]
-                else:
-                    self.vars[v] = divisor.vars[v]
+                if (self_expo := self.vars.get(v)):
+                    if (self_expo < divisor.vars[v]):
+                        divisor.vars[v] -= self.vars[v]
+                        del self.vars[v]
+
+                    elif (self_expo > divisor.vars[v]):
+                        self.vars[v] -= divisor.vars[v]
+                        del divisor.vars[v]
+
+                    elif (self_expo == divisor.vars[v]):
+                        del self.vars[v]
+                        del divisor.vars[v]
+
+def __record_vars(exp: str) -> tuple[list[str], list[int]]:
+    """Parse the variables from a string expression. Records variables and exponents into separate lists.
+
+    Args:
+        exp: The mathematical expression.
+
+    Returns:
+        A list of variables and a list of their corresponding exponents.
+    """
+
+    variables_l = []
+    exponents_l = []
+
+    vars_len = len(exp)
+
+    exponent_counter = ""
+    
+    for i in range(vars_len):
+        curr = exp[i]
+
+        if curr.isalpha():
+            variables_l.append(curr)
+
+            if exponent_counter:
+                exponents_l.append(int(exponent_counter))
+                exponent_counter = ""
+
+            if (i+1 == vars_len) or (exp[i+1] != "^"):
+                exponents_l.append(1)
 
 
+        elif curr.isnumeric() and ((i > 0) and ((exp[i-1] == "^") or exponent_counter)):
+            exponent_counter += curr
+
+            if (i+1 == vars_len):
+                exponents_l.append(int(exponent_counter))
+
+    return variables_l, exponents_l
+
+def __parse_vars(exp: str) -> dict[str, int]:
+    """Parse the variables from a string expression. Creates a dict to store the variables and their respective exponents. The dict is sorted.
+
+    Args:
+        exp: The Mathematical expression.
+
+    Returns:
+        The dictionary contaning the variables (Keys) and their exponents (Value).
+    """
+    vars_d = {}
+    variables_l, exponents_l = __record_vars(exp)
+
+    for v, e in zip(variables_l, exponents_l):
+        vars_d[v] = e
+
+    return dict(sorted(vars_d.items()))
+
+def parse_exp(exp: str):
+    coeff = __parse_coeff(exp)
+    vars = __parse_vars(exp)
+    div = __parse_div(exp)
+
+def __parse_div(exp: str):
+    i = exp.find("/")
+    
+    if (i == -1):
+        return 1
+
+    coeff = __parse_coeff(exp[i])
+    vars  = __parse_vars(exp[i])
+
+    term = term(coeff, vars)
+    return term
+
+def __parse_div(self, div: str):
+    int_d = int(div)
+    float_d = float(div)
+    return (int_d if (int_d == float_d) else float_d)
+
+
+# Extras
+
+def __is_divisible(dividend: int, divisor: int) -> bool:
+    q1 = dividend/divisor
+    q2 = dividend//divisor
+
+    return q1 == q2
+
+def __simplify_fraction(numerator: int, denominator: int):
+    gcf = __find_GCF(numerator, denominator)
+
+    return numerator//gcf, denominator//gcf
+
+def __find_GCF(n1: int, n2: int):
+    n1_factors = __find_factors(n1)
+    n2_factors = __find_factors(n2)
+
+    n1_flen = len(n1_factors)
+    n2_flen = len(n2_factors)
+
+    i = j = 1
+
+    gcf = 1
+    while ((i+1 < n1_flen) or (j+1 < n2_flen)):
+        if (n1_factors[i] == n2_factors[j]):
+            gcf = n1_factors[i]
+            i += 1
+            j += 1
+
+        if n1_factors[i] < n2_factors[j]:
+            i += 1
+        elif n1_factors[i] > n2_factors[j]:
+            j += 1
+
+    return gcf
+
+def __find_factors(n: int) -> list[int]:
+    i = 1
+
+    factors = []
+    while (i**2 <= n):
+        q1 = n/i
+        q2 = n//i
+        if (q1 == q2):
+            factors.append(i)
+            factors.append(q2)
+
+        i += 1
+    return sorted(factors)
 if __name__ == "__main__":
     main()
