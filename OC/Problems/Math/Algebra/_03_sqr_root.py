e6723696c4a380822348358e2395003d4f50433b
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2026-04-18 13:40:58 +0400

    Add decimal place name when rounding

--- OC/Problems/Math/Algebra/_03_sqr_root.py
+++ OC/Problems/Math/Algebra/_03_sqr_root.py
@@ -1,12 +1,24 @@
 from math import sqrt
 
+# Index values, and integers.
 THOUSANDTH = 3
 H_THOUSANDTH = 5
+
 NDIGITS = THOUSANDTH
+
+
+STR_ROUNDING = [
+    "Tenths",
+    "Hundredths",
+    "Thousandths",
+    "Ten Thousandths"
+    "Hundred Thousandths"
+]
+
 
 def main() -> None:
     x = float(input("Enter number:\n"))
     out = round(sqrt(x), NDIGITS)
-    print(f"√{x} = {out}")
+    print(f"√{x} (Nearest {STR_ROUNDING[NDIGITS-1]}) ≈ {out}")
 if __name__ == "__main__":
     main()
