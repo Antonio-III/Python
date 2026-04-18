395e31eda39b50307be30ebd11c50daed9aa2668
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2026-04-18 13:41:43 +0400

    Add new fsum function for quadratic expressions

--- OC/Problems/Math/Arithmetic/_03_factors_sum.py
+++ OC/Problems/Math/Arithmetic/_03_factors_sum.py
@@ -1,15 +1,14 @@
 from _04_gcf import find_factors
 
 def main() -> None:
-    x = input("Enter quadratic coefficient and constant:\n").split()
-    y = int(input("Target sum:\n"))
+    factors = input("Enter quadratic coefficient and constant:\n").split()
+    assert(len(factors) == 2)
 
-    prod = 1
-    for i in x:
-        prod *= int(i)
-    
-    factors = find_factors(abs(prod), unique=False)
-    print(factors_sum(factors, y))
+    x, y = (int(n) for n in factors)
+
+    z = int(input("Target sum:\n"))
+
+    print(factors_sum_quadra(x, y, z))
 
 def factors_sum(factors: list[int], x: int) -> tuple[int, int]:
     
@@ -26,7 +25,27 @@ def factors_sum(factors: list[int], x: int) -> tuple[int, int]:
             if (-f1 - f2 == x):
                 return (-f1, -f2)
 
-    return (-1, -1)
+    return (0, 0)
+
+def factors_sum_quadra(x: int, y: int, z: int) -> tuple[int, int]:
+    prod = x*y
+    factors = find_factors(prod, unique=False)
+
+    for i in range(len(factors)//2):
+        f1, f2 = factors[i], factors[-(i + 1)]
+        if prod >= 0:
+            if (f1 + f2 == z):
+                return (f1, f2)
+            if (-f1 - f2 == z):
+                return (-f1, -f2)
+        elif prod < 0:
+            if (f1 - f2 == z):
+                return (f1, -f2)
+            if (-f1 + f2 == z):
+                return (-f1, f2)
+
+    return (0, 0)
+
 
 if __name__ == "__main__":
     main()
