3f9c6e1279d041b997e70df9f87773fd724c0256
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2026-07-04 13:05:22 +0400

    _05_p_factorization now relies in files for primes - The script now uses a text file for storing prime numbers.

--- OC/Problems/Math/Arithmetic/_05_p_factorization.py
+++ OC/Problems/Math/Arithmetic/_05_p_factorization.py
@@ -1,15 +1,22 @@
 from _04_gcf import find_factors, __is_divisible
 
+# Relative to Math directory.
+PRIME_DIR = r"Arithmetic\Text\prime_numbers.txt"
+
 def main():
     x = input("Enter number:\n")
     if (not x.isnumeric()):
         print("Input must be a number.")
         return
     x = int(x)
-    assert(x > 0)
+    assert(x > 1)
 
     p_factors = p_factorization(x)
-    print(p_factors)
+    if (p_factors):
+
+        print(p_factors)
+    else:
+        print(f"{x} is prime.")
 
     return None
 
@@ -23,23 +30,37 @@ def p_factorization(n: int) -> dict[int, int]:
         A dictionary containing the prime factors (keys) of `n` in ascending order and its exponents (values).
     """
     p_factors = {}
-    prime = 2
+    primes = __get_prime_nums()
+    # The text file should not be empty
+    assert(primes)
+
+    primes_len = len(primes)
+    ptr = 0
 
     while not (is_prime(n)):
-        if (__is_divisible(n, prime)):
-            n = n//prime
+        if (ptr == primes_len):
+            primes.append(next_prime(primes[ptr-1]))
+            primes_len+= 1
+
+        if (__is_divisible(n, primes[ptr])):
+            # Perform division
+            n = n//primes[ptr]
+
             # Record divisor
-            p_factors = record_number(p_factors,prime)
+            p_factors = __record_number(p_factors, primes[ptr])
             
             # Record quotient
             if (is_prime(n)):
-                p_factors = record_number(p_factors, n)
+                p_factors = __record_number(p_factors, n)
+                break
 
             # Reset the divisor
-            prime = 2
-        else:
-            prime = next_prime(prime)
+            ptr = 0
 
+        else:
+            ptr+= 1
+
+    __update_prime_txt(primes)
     return p_factors
 
 def is_prime(n: int) -> bool:
@@ -63,11 +84,30 @@ def next_prime(n: int) -> int:
 
     return n
 
-def record_number(p_factors: dict[int, int], n: int):
+def __record_number(p_factors: dict[int, int], n: int):
     if not (p_factors.get(n)):
         p_factors[n] = 1
     else:
         p_factors[n]+= 1
     return p_factors
+
+def __get_prime_nums() -> list[int]:
+    with open(PRIME_DIR, "r") as primes_txt:
+        file_contents = primes_txt.read()
+        primes = [int(num) for num in file_contents.strip().split(",")]
+    
+    return primes
+
+def __update_prime_txt(primes: list[int]) -> None:
+    primes_old = __get_prime_nums()
+    primes_new = [num for num in primes if not (num in primes_old)]
+
+    new_data = ",".join(map(str, primes_new))
+    
+    with open(PRIME_DIR, "a") as prime_txt:
+        prime_txt.write(f",{new_data}")
+
+    return
+
 if __name__ == "__main__":
     main()
